49f5ce6ccec79b2e3d6b1c628657aadd112b8341
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-12 09:51:20 -0700

    testing for correct parsing behavior on subset of data

--- Bumblebees/bee_wrangle.py
+++ Bumblebees/bee_wrangle.py
@@ -7,15 +7,12 @@ with open('testdump.json') as data_file:
 keepers = ["bee_id", "common_name", "floral_host", "latitude",
            "longitude", "dateidentified"]
 
-final_list = []
 
-for index, entry in enumerate(data["data"]["data"]):
+for entry in data["data"]["data"]:
     keeper_attrs = {}
     for elmnt in entry:
         if elmnt in keepers:
             keeper_attrs[elmnt] = entry[elmnt]
-    final_list.append({index:keeper_attrs})
-
-for item in final_list:
-    print(item)
+    data = {entry["bee_id"]:keeper_attrs}
+    print (data)
     print()
