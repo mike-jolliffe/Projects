6ae24d4aecbabba143035362aa262d365d5b069c
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-11 22:17:45 -0700

    small script to manipulate json into form I need

--- Bumblebees/beescrape.py
+++ Bumblebees/beescrape.py
@@ -20,27 +20,30 @@ class XercesScraper:
         response = requests.get(page)
 
         # We want the first element of response, under key 'data'
-        return response.json()['data']
+        return response.json()
 
     def parse_sightings(self, data):
-        # TODO clean up response JSON, only keeping bee_id, common_name, floral_host,
-        #      latitude, longitude, and dateidentified
-        pass
+        # # TODO clean up response JSON, only keeping bee_id, common_name, floral_host,
+        # #      latitude, longitude, and dateidentified
+        keepers = ["bee_id", "common_name", "floral_host", "latitude",
+                   "longitude", "dateidentified"]
+        data = {k:v for k in data["data"]["data"][0].items() if k in keepers}
+        return data
 
     def run(self):
         for page in range(1,17):
             # Retrieve data for page
             data = self.get_bee_sightings(self.API_url_front + str(page) + self.API_url_back)
             print ('scraped page {}'.format(str(page)))
-            # TODO identify the exact elements I want to grab. Full dump for now
-            self.parse_sightings(data)
-            self.scraped_pages.append(data)
+            keeper_data = self.parse_sightings(data)
+            print(keeper_data)
+            self.scraped_pages.append(keeper_data)
 
         self.save_data()
 
     def save_data(self):
         with open('bee_sightings.json', 'w') as json_file:
-            json.dump(self.scraped_pages, json_file, indent=4)
+            json.dump(self.scraped_pages, json_file, indent=2)
 
 if __name__ == '__main__':
     scraper = XercesScraper()
