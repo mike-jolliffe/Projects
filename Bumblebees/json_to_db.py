226d8d2348c30d3e97e2aacc5be9e6f49715a546
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-08-18 12:08:14 -0700

    created file to decode JSON, manipulate data into desired structure, and insert into SQLite database

---
+++ Bumblebees/json_to_db.py
@@ -0,0 +1,36 @@
+# imports
+import sqlite3
+import requests
+import json
+
+# decode the json
+with open('bee_sightings.json') as fh:
+    js = json.load(fh)
+
+# clean up the dictionary by eliminating the outermost key
+data = []
+for item in js:
+    data.extend([value for (key,value) in item.items()])
+
+# flatten dateidentified from dict of dicts, grab just date
+for item in data:
+    item["dateidentified"] = item["dateidentified"]["date"]
+
+# create a new database file and generate a cursor for SQL execution
+conn = sqlite3.connect('bee_sightings.sqlite')
+cur = conn.cursor()
+
+# create a table for the data
+cur.execute('''CREATE table bee_sightings(bee_id INTEGER, common_name TEXT,
+               dateidentified DATETIME, latitude REAL, longitude REAL,
+               floral_host TEXT, sightingstatus_id INTEGER)''')
+
+# Insert data into the table
+cur.executemany('''INSERT INTO bee_sightings(bee_id, common_name, dateidentified,
+                  latitude, longitude, floral_host, sightingstatus_id) VALUES
+                  (:bee_id, :common_name, :dateidentified, :latitude, :longitude,
+                  :floral_host, :sightingstatus_id)''', data)
+
+# Save the changes and close the connection
+conn.commit()
+conn.close()
