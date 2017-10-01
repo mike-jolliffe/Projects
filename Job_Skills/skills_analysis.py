77c06f27919c168136bdd557aa19e117fa9100cf
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-10-01 10:06:02 -0700

    added get_count() to create frequency dict

--- Job_Skills/skills_analysis.py
+++ Job_Skills/skills_analysis.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import json
 import matplotlib.pyplot as plt
 
@@ -8,22 +9,43 @@ class DataPrepper:
         self.skills_list = None
 
     def from_json(self):
-        '''Opens JSON file and deserializes JSON object'''
+        """Opens JSON file and deserializes JSON object"""
         with open(self.file) as json_infile:
             jobs_list = json.load(json_infile)
         return jobs_list
 
     def clean_strings(self, lists):
-        '''Breaks up list of single-element lists into list of multi-element lists'''
+        """Breaks up list of single-element lists into list of multi-element lists"""
         cleaned = []
         for lst in lists:
             new_lst = list(lst[0].split(', '))
             cleaned.append(new_lst)
 
         self.skills_list = cleaned
-
+        return self.skills_list
 
 class Visualizer:
+    def __init__(self, skills):
+        self.skills = skills
+        self.word_count = Counter()
+
+    def flatten(self):
+        """
+        Flattens list of lists to list of strings
+        :return: List of strings
+        """
+        self.skills = [item for sublist in self.skills for item in sublist]
+        return self.skills
+
+    def get_count(self):
+        """
+        Creates dictionary with skills as keys, number of occurrences as values
+        :return: dictionary
+        """
+        for word in self.skills:
+            self.word_count[word] += 1
+        return self.word_count
+
     '''TODO Make frequency histogram, visualize skills, and return list of top 5 desired skills along with percent of
             employers desiring those skills
         TODO Use networkx to build a network graph of skills. Try to detect any present communities in graph representing
@@ -32,8 +54,10 @@ class Visualizer:
 
 if __name__ == '__main__':
     data = DataPrepper('jobs.json')
-    data.clean_strings(data.from_json())
-    print(data.skills_list)
+    clean = data.clean_strings(data.from_json())
+    visualize = Visualizer(clean)
+    visualize.flatten()
+    print(visualize.get_count())
 
 
 
