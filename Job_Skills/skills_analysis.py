5bd27223da356ea3120b3d1edeebbaf256e38ba1
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-30 17:40:59 -0700

    new file with TODOs for analyzing scraped jobs data

---
+++ Job_Skills/skills_analysis.py
@@ -0,0 +1,5 @@
+''' TODO import scraped job skills from jobs.json file
+    TODO Make frequency histogram, visualize skills, and return list of top 5 desired skills along with percent of
+        employers desiring those skills
+    TODO Use networkx to build a network graph of skills. Try to detect any present communities in graph representing
+        an ecosystem or stack of tools commonly desired together.'''
