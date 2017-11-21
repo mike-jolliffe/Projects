04380e56325e144464d496d41d35d5bb6085d686
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-11-21 14:28:01 -0800

    make heatmap of bee-flower interactions

---
+++ Bumblebees/python_snippet.py
@@ -0,0 +1,7 @@
+{
+ 'bee_id': 21041,
+ 'common_name': 'Yellow-faced bumble bee',
+ 'dateidentified': '2017-08-05 00:00:00.000000',
+ 'floral_host': None, 'latitude': '45.524403',
+ 'longitude': '-122.637095', 'sightingstatus_id': 1
+ }
